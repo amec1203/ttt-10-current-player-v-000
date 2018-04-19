4cce646c09403c2d4a00c7e2cbae2f845a250a7d
Angela Campion <campion.angela@gmail.com>
2018-04-19 19:49:00 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,10 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 player_1 = "X"
 play_2 = "Y"
-turns_taken = 0
+turn_count = 0
+
+loop do
+  if turn_count < turns_played
 
 counter = 0
 moves.each do | move|
