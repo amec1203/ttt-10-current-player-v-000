58603ffbe746f7d639748668bf451a014cc18c1c
Angela Campion <campion.angela@gmail.com>
2018-04-19 20:42:37 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,7 +4,11 @@ def turn_count(board)
     if move == "X" || move == "O"
       count += 1
   end
-end 
+end
 
 def current_player(board)
-  
+  if turn_count % 2
+    puts "X"
+  else 
+  end
+end 
