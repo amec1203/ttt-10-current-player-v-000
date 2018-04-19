c346b76162c1b7c9397112a2469a1831c0224dd5
Angela Campion <campion.angela@gmail.com>
2018-04-19 19:23:04 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,19 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
 #turn_count
 # player X goes first, so if only one space filled, X
 # player O goes second, so if two spaces filed, O
 .each
-plays = ["X", "O"]
+game_plays = ["X", "O"]
+game_plays.each do |play|
+  
 counter = 0
 if space_occupied
   counter += 1
