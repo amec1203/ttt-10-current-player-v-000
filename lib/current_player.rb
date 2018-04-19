1055cd507fe9368426dfbd032f0fe756be551ba8
Angela Campion <campion.angela@gmail.com>
2018-04-19 20:09:11 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,5 @@
 spaces = ["space 1", "space 2", "space 3", "space 4", "space 5", "space 6", "space 7", "space 8", "space 9"]
 counter = 0
-space.each do |space|
+spaces.each do |space|
   counter += 1
 end
