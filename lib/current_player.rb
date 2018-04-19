ed6ee010f97d26049cd0e612489f33aaaccbc748
Angela Campion <campion.angela@gmail.com>
2018-04-19 19:13:23 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,12 @@
 #turn_count
+# player X goes first, so if only one space filled, X
+# player O goes second, so if two spaces filed, O
+.each 
 
+counter = 0
+if space_occupied
+  counter += 1
+end
 
 
 #current_player
