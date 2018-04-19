c4711d98cd0d3bf85a46dc53695c6587bdfe6621
Angela Campion <campion.angela@gmail.com>
2018-04-19 19:53:49 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,8 @@
-turns available = 9
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 turn count = 0
 
 loop do
-  if turn_count < turns_available
+  if turn_count < board
     turn_count += 1
   else
     break
